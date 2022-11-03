a1e3289ff8fdfb79436e22ee8f1576aeb993a4e8
nanji <nanji@DESKTOP-4A8MIDK>
2022-11-03 21:04:12 +0300

    Add project files.

---
+++ Exercise 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "sunny")
+            {
+                Console.WriteLine("its warm!");
+            }
+            else
+            {
+                Console.WriteLine("its cold!");
+            }
+            
+
+        }
+    }
+}
